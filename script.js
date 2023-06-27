8bacea416dcd57c380d09eaf963fc3269b5b3163
sudila <sudilamunasinghe@gmail.com>
2023-06-27 18:24:16 +0530

    Updated

--- script.js
+++ script.js
@@ -24,25 +24,48 @@ let products = [];
     function proceed() {
       let output = '<h2>Order Summary</h2>';
       let totalPrice = 0;
-      
+      document.getElementById('form').style.display = "none";
       if (products.length === 0) {
         output += '<p>No products added.</p>';
-        document.getElementById('output').style.display = "block";
+        document.getElementById('output').style.visibility = "visible";
+        document.getElementById('output').style.top = "50%";
       } else {
         output += '<table>';
         output += '<tr><th>Product Name</th><th>Quantity</th><th>Unit Price</th><th>Sub Total</th></tr>'
         products.forEach(function(product) {
           const subtotal = product.price * product.quantity;
           totalPrice += subtotal;
-          output += `<tr><td>${product.name}</td><td>${product.quantity}</td><td>${product.price.toFixed(2)}</td><td>${subtotal.toFixed(2)}</td></tr>`;
+          output += `<tr><td>${product.name}</td><td>${product.quantity}</td><td>Rs.${product.price.toFixed(2)}</td><td>Rs. ${subtotal.toFixed(2)}</td></tr>`;
         });
         
-        output += `<tr><th>Total Price${totalPrice.toFixed(2)}</th></tr>`;
+        output += `<tr><th>Total Price</th><td></td><td></td><td>Rs.${totalPrice.toFixed(2)}</td></tr>`;
         output += '</table>';
-        document.getElementById('output').style.display = "block";
-        
+        output += '<a href="print.html" target="_blank"><input type="button" value="Print" id="print"></a>'
+        document.getElementById('output').style.visibility = "visible";
+        document.getElementById('output').style.top = "50%";
       }
       
       
       document.getElementById('output').innerHTML = output;
+      function print(){
+        let output_p = '<h2>Order Summary</h2>';
+      let totalPrice = 0;
+      
+      if (products.length === 0) {
+        output_p += '<p>No products added.</p>';
+      } else {
+        output_p += '<table>';
+        output_p += '<tr><th>Product Name</td><td>Quantity</td><td>Unit Price</td></tr>'
+        products.forEach(function(product) {
+          const subtotal = product.price * product.quantity;
+          totalPrice += subtotal;
+          output_p += `<tr><td>${product.name}</td><td>${product.quantity}</td><td>$${product.price.toFixed(2)}</td><td>$${subtotal.toFixed(2)}</td></tr>`;
+        });
+        output_p += '</table>';
+        
+        output_p += `<p>Total Price: $${totalPrice.toFixed(2)}</p>`;
+      }
+      document.getElementById('output_p').innerHTML = output_p;
     }
+  }
+    
