500bf71fd233cb5fafe029d10e7510f6965aa88d
sudila <sudilamunasinghe@gmail.com>
2023-06-29 11:29:14 +0530

    Minor Changes

--- script.js
+++ script.js
@@ -22,10 +22,11 @@ let products = [];
     }
     
     function proceed() {
-      let output = '<i class="fa-solid fa-xmark" id="close"></i>';
+      let output = '<i class="fa-solid fa-xmark"></i>';
       output += '<h2>Order Summary</h2>';
       let totalPrice = 0;
       document.getElementById('form').style.display = "none";
+
       if (products.length === 0) {
         output += '<p>No products added.</p>';
         document.getElementById('output').style.visibility = "visible";
@@ -38,9 +39,11 @@ let products = [];
           totalPrice += subtotal;
           output += `<tr><td>${product.name}</td><td>${product.quantity}</td><td>Rs.${product.price.toFixed(2)}</td><td>Rs. ${subtotal.toFixed(2)}</td></tr>`;
         });
-        
         output += `<tr><th>Total Price</th><td></td><td></td><td id="tot">Rs.${totalPrice.toFixed(2)}</td></tr>`;
         output += '</table>';
+        output += '<h3 class="tot">Total Price : Rs. ';
+        output += totalPrice.toFixed(2);
+        output += '</h3>'
         output += '<input type="button" id="checkout" value="Checkout">'
         document.getElementById('output').style.visibility = "visible";
         document.getElementById('output').style.top = "50%";
@@ -55,24 +58,32 @@ let products = [];
         document.getElementById('output').innerHTML = output;
         console.log(output);
         document.getElementById('checkout').style.display = "none";
+
+        function next(){
+          var total = totalPrice.toFixed(2);
+          let customer = document.getElementById('customer').value;
+          var remain = customer - total;
+          output += '<h3 class="paid">Paid Amount : Rs. ';
+          output += customer;
+          output += '</h3>'
+          output += '<h3 class="remaining">Remaining Balance : Rs. ';
+          output += (remain);
+          output += '</h3>';
+          document.getElementById('output').innerHTML = output;
+          document.getElementById('next').style.display = "none";
+          document.getElementById('checkout').style.display = "none";
+          document.getElementById('customer').style.display = "none";
+        }
+        document.getElementById('next').addEventListener('click', next);
       }
       document.getElementById('checkout').addEventListener('click', check);
       
-      
     }
-    function next(){
-      var total = totalPrice.toFixed(2);
-      var customer = parseInt(document.getElementById('customer').value);
-      var remain = total - customer;
-      console.log('Sudila Munasinghe');
-      console.log(remain);
-      // output += '<h2>Remaining Balance :';
-      // output += (remain);
-      // output += '</h2>'
-      // var checkout = parseInt(document.getElementById('checkout').value);
-      // var total = totalPrice.toFixed(2);
-      // var remain = total - checkout;
-      
-    }
-    document.getElementById('next').addEventListener('click',next);
+    // $('#close').click(function(){
+    //   $('#output').css({
+    //     top: '40%',
+    //     visibility: 'hidden',
+    //   })
+    // })
+    
     
